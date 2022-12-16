b88b4f2f86e0c63738daf0e31f09839b948b93fa
srivijayalakshmi.AT <114483681+21viji@users.noreply.github.com>
2022-12-16 09:43:04 +0000

    problem04 done + notes

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,26 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int input();
-int add(int a, int b);
+void add(int a, int b, int *sum);
 void output(int a, int b, int sum);
-int main(){
-  int a, b, sum;
-  printf("enter two no's: ");
-  a = input();
-  b = input();
-  sum = add(a, b);
-  output(a, b, sum);
+
+int input()
+{
+ int n;
+  printf("enter first number:\n");
+  scanf("%d",&n);
+  return n;
+
+  }
+
+void add(int a, int b, int *sum)
+{
+*sum=a+b;
+  }
+
+void output(int a , int b , int sum)
+{
+  printf("the sum of %d and %d is %d" ,a,b,sum);
 }
-int input(){
-  int a;
-  scanf("%d", &a);
-  return a;
+
+int main()
+{
+ int a,b,sum;
+  a=input();
+  b=input();
+  add(a,b,&sum);
+  output(a,b,sum);
 }
-int add(int a, int b){
-  int sum;
-  sum = a + b;
-  return sum;
-}
- void output(int a, int b, int sum){
-   printf("The sum of %d and %d is: %d\n", a, b,sum);
- 
-   }
+
+//its pass by value for a and b
+//its pass by reference for sum
