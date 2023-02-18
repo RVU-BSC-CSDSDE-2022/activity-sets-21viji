a5197bcb3c2695a268225c6ff6aecb5333b91ae1
srivijayalakshmi.AT <114483681+21viji@users.noreply.github.com>
2023-02-18 05:47:09 +0000

    clear

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,12 +1,22 @@
-#include<stdio.h>
 
-int main()
-{
-  int a, b, sum;    //declaring the variables used in that main function.
-  printf("enter two no's: \n");
-  scanf("%d%d", &a,&b);
-  sum = a + b;
-  printf("The sum of %d and %d is %d", a, b, sum); 
+
+
+
+
+
+
+
+
+
+// #include<stdio.h>
+
+// int main()
+// {
+//   int a, b, sum;    //declaring the variables used in that main function.
+//   printf("enter two no's: \n");
+//   scanf("%d%d", &a,&b);
+//   sum = a + b;
+//   printf("The sum of %d and %d is %d", a, b, sum); 
   
-  //a,b,sum should mentioned to that %d belongs to which chracter.
-}
+//   //a,b,sum should mentioned to that %d belongs to which chracter.
+// }
