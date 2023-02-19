6fcd716f6cd065ebd9e631f7e1b37ea2c2a818d6
srivijayalakshmi.AT <114483681+21viji@users.noreply.github.com>
2023-02-19 04:13:11 +0000

    set 1 prob 2 done

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,sum;                                       
+  printf("enter a value");
+  scanf("%d",&a);
+  printf("enter b value");
+  scanf("%d",&b);
+  sum = a+b;
+  printf("the sum of %d and %d is %d",a,b,sum);
+  return 0;
+     } 
+
+
 
 
 
